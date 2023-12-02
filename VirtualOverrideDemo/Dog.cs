73e3acb13a558576852eddcbd538324571faa074
DejnielST <dejni@DESKTOP-CQ726VA>
2023-12-02 17:24:08 +0100

    Inheritance test

---
+++ VirtualOverrideDemo/Dog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace VirtualOverrideDemo
+{
+    internal class Dog : Animal
+    {
+        public bool IsHappy { get; set; }
+        public Dog(string name, int age):base(name , age)
+        {
+            IsHappy = true;
+        }
+
+        public override void Eat()
+        {
+            base.Eat();
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Wuuuuufff !");
+        }
+
+        public override void Play()
+        {
+            if (IsHappy)
+            {
+                base.Play();
+            }
+            
+        }
+    }
+}
---
+++ VirtualOverrideDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace VirtualOverrideDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog D1 = new Dog("Doggy",10);
+            Console.WriteLine($"{D1.Name} is {D1.Age}");
+            D1.Play();
+            D1.MakeSound();
+            D1.Eat();
+        }
+    }
+}
