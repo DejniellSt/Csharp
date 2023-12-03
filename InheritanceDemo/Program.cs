bf1858b052d2e9f6275e8e696bc80d0832f62e0c
DejnielST <dejni@DESKTOP-CQ726VA>
2023-12-03 14:13:38 +0100

    Added new file VideoPost.cs, modified Program.cs

--- InheritanceDemo/Program.cs
+++ InheritanceDemo/Program.cs
@@ -10,6 +10,14 @@
             ImagePost imageP1 = new ImagePost("Check my new ps4 games","Dejniel S","https://testsite.com",true);
             Console.WriteLine(imageP1.ToString());
 
+            VideoPost v1 = new VideoPost("FailVideo", "Dejniel", "https://testsite.com", true, 10);
+            Console.WriteLine(v1.ToString());
+
+            v1.Play();
+            Console.WriteLine("Press any key to stop video");
+            Console.ReadKey();
+            v1.Stop();
+
             Console.ReadLine();
         }
     }
---
+++ InheritanceDemo/VideoPost.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace InheritanceDemo
+{
+    internal class VideoPost : Post
+    {
+        // Fields 
+        protected bool isPlaying = false;
+        protected int currentDuration = 0;
+        Timer timer;
+        // Properties
+        protected string VideoURL { get; set; }
+        protected int Length { get; set; }
+
+        public VideoPost() { }
+
+        public VideoPost(string title, string sendByUsername, string videoURL, bool isPublic, int length)
+        {
+            this.ID = GetNextID();
+            this.Title = title;
+            this.SendByUsername = sendByUsername;
+            this.IsPublic = isPublic;
+            this.Length = length;
+            this.VideoURL = videoURL;
+        }
+
+        public void Play()
+        {
+            if (!isPlaying)
+            {
+                isPlaying = true;
+                Console.WriteLine("Playing");
+                timer = new Timer(TimerCallback, null, 0, 1000);
+            }
+        }
+
+        private void TimerCallback(Object o)
+        {
+            if(currentDuration < Length)
+            {
+                currentDuration++;
+                Console.WriteLine("Video at {0}s", currentDuration);
+                GC.Collect(); // clean up any mess 
+            }
+            else
+            {
+                Stop();
+            }
+        }
+
+        public void Stop()
+        {
+            if (isPlaying)
+            {
+                isPlaying=false;
+                Console.WriteLine("Stopped at {0}", currentDuration);
+                currentDuration = 0;
+                timer.Dispose();
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1} - {2} - {3} - by {4}", this.ID, this.Title, this.VideoURL,this.Length, this.SendByUsername);
+        }
+    }
+}
