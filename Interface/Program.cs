dd578d5626e8271797d9232da70de2fd7dbe18c7
DejnielST <dejni@DESKTOP-CQ726VA>
2023-12-03 18:34:29 +0100

    Interface test

---
+++ Interface/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ticket t1 = new Ticket(10);
+            Ticket t2 = new Ticket(10);
+            Console.WriteLine(t2.Equals(t1));
+
+            Ticket t3 = new Ticket(10);
+            Ticket t4 = new Ticket(4);
+            Console.WriteLine(t3.Equals(t4));
+        }
+    }
+}
