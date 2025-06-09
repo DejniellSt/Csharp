7d3fd1aa5cb544b9b1380dd8172ad42ea36215a4
Daniel S <38562325+DejniellSt@users.noreply.github.com>
2025-06-09 12:06:09 +0200

    Added file to repository

---
+++ CS_Nullable/CS_basic/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CS_basic;
+
+class Program
+{
+    // Nullable
+    public class Uzytkownik
+    {
+        public string Imie { get; set; }
+        public DateTime? DataOstatniegoLogowania { get; set; }
+    }
+
+    static void Main(string[] args)
+    {
+        Uzytkownik u1 = new Uzytkownik
+        {
+            Imie = "Testowe",
+            // DataOstatniegoLogowania = null;
+            DataOstatniegoLogowania = DateTime.Now
+        };
+
+
+        if (u1.DataOstatniegoLogowania.HasValue)
+        {
+            Console.WriteLine($"Ostatnie logowanie: {u1.DataOstatniegoLogowania.Value}");
+        }
+        else
+        {
+            Console.WriteLine("Brak logowania");
+        }
+
+    }
+}
+  
+
---
+++ EX_abstract/EX_abstract/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace EX_abstract;
+
+
+public abstract class Vehicle
+{
+    public string Model { get; set; }
+    public Vehicle (string model)
+    {
+        Model = model;
+        Console.WriteLine(model);
+    }
+    public abstract void Start();
+    public abstract void Stop();
+}
+
+public class Car : Vehicle
+{
+    public Car(string model) : base(model) { }
+    public override void Start()
+    {
+        Console.WriteLine("Car start");
+    }
+
+    public override void Stop()
+    {
+        Console.WriteLine("Car stop");
+    }
+}
+
+public class Motor : Vehicle
+{
+    public Motor(string model) : base(model) { }
+    public override void Start()
+    {
+        Console.WriteLine("Motor start");
+    }
+    public override void Stop()
+    {
+        Console.WriteLine("Car stop");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car c1 = new Car("Tesla");
+        c1.Start();
+        c1.Stop();
+
+        Motor m1 = new Motor("HX1");
+        m1.Start();
+        m1.Stop();
+    }
+}
+
