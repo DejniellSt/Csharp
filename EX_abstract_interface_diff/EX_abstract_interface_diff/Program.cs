e62f54ed013dd0a9388da9c52232c041deaebd23
Daniel S <38562325+DejniellSt@users.noreply.github.com>
2025-06-24 11:11:32 +0200

    Add files via upload
    Added files to repository. Abstract class vs interfaces example and explicit and implicit difference

---
+++ EX_abstract_interface_diff/EX_abstract_interface_diff/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace EX_abstract_interface_diff;
+
+class Program
+{
+    // Interface
+    interface Timer
+    {
+        public void DisplyTime();
+    }
+
+    // Abstract class  
+    abstract class BaseClock
+    {
+        public string Marka { get; set; }
+
+        public abstract void SetTime(DateTime time);
+
+        public void ShowMark()
+        {
+            Console.WriteLine($"Marka : {Marka}");
+        }
+    }
+
+    // Base class
+    class DigitalClock : BaseClock, Timer
+    {
+        private DateTime timeNow;
+
+        public override void SetTime(DateTime time)
+        {
+            timeNow = time; 
+        }
+
+        public void DisplyTime()
+        {
+            Console.WriteLine($"Time now: {timeNow:HH:MM:ss}");
+        }
+
+    }
+
+
+    static void Main(string[] args)
+    {
+        DigitalClock d1 = new DigitalClock();
+        d1.Marka = "Rolex";
+        d1.SetTime(DateTime.Now);
+
+        d1.ShowMark(); // z klasy abstrakcyjnej
+        d1.DisplyTime(); // z interfejsu 
+    }
+}
---
+++ Ex_explicit_implicit_diff/Ex_explicit_implicit_diff/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Ex_explicit_implicit_diff;
+
+class Program
+{
+    // Implicit interface 
+    interface Implicit
+    {
+        public void Test_Im();
+    }
+
+    // Explicit interface 
+    interface Explicit
+    {
+        public void Test_Ex();
+    }
+
+    public class Coffe : Implicit
+    {
+        public void Test_Im() // implicit implementation 
+        {
+            Console.WriteLine("Coffe");
+        }
+    }
+
+    public class Water : Explicit
+    {
+        void Explicit.Test_Ex() // explicit implementation 
+        {
+            Console.WriteLine("Water");
+        }
+    }
+
+    // Conflict between interfaces with the same method
+    public interface IA
+    {
+        void Show();
+    }
+
+    public interface IB
+    {
+        void Show();
+    }
+
+    public class MyClass : IA, IB
+    {
+        void IA.Show()
+        {
+            Console.WriteLine("IA");
+        }
+
+        void IB.Show()
+        {
+            Console.WriteLine("IB");
+        }
+    }
+
+
+    static void Main(string[] args)
+    {
+        // Implicit
+        Coffe c1 = new Coffe();
+        c1.Test_Im();
+
+        // Explicit
+        Water w1 = new Water();
+        //w1.Test_Ex();// error
+        ((Explicit)w1).Test_Ex(); // Correct
+
+        // Conflicts example
+        MyClass m1 = new MyClass();
+        IA a = m1;
+        IB b = m1;
+
+        a.Show();// method IA
+        b.Show();// method IB
+
+        // other way ...
+        ((IA)m1).Show();
+        ((IB)m1).Show();
+    }
+}
+
