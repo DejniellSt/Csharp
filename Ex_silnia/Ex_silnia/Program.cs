621890b87b75a70c29b382a63694b5326aec2df7
Daniel S <38562325+DejniellSt@users.noreply.github.com>
2025-06-25 13:22:26 +0200

    Add files via upload
    Added excercise "Ex_Silnia" to the repository

---
+++ Ex_silnia/Ex_silnia/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FactorialExample
+{
+    class Factorial
+    {
+        public int MaxN { get; }
+
+        public Factorial(int maxN) => MaxN = maxN;
+
+        private int CalculateFactorial(int n) => (n <= 1) ? 1 : n * CalculateFactorial(n - 1);
+
+        public void Show()
+        {
+            for (int i = 0; i < MaxN; i++)
+            {
+                Console.WriteLine($"Iteration {i} : value {CalculateFactorial(i)}");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factorial f = new Factorial(7);
+            f.Show();
+        }
+    }
+}
